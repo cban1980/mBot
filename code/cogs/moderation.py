fcae22bb39322756bc5a882e4e6e6e93d3204fd8
cban1980 <cban@gmx.com>
2024-01-08 04:30:25 +0100

    update

--- code/cogs/moderation.py
+++ code/cogs/moderation.py
@@ -1,11 +1,12 @@
 # Name: moderation.py
-# Description: Moderation slash commands for mBot.
+"""Cog relating to moderation commands and functions."""
 
 import nextcord
 from nextcord.ext import commands
 from nextcord import Interaction
 
 class moderation(commands.Cog):
+    """Moderation commands and functions."""
     def __init__(self, bot):
         super().__init__()
         self.bot = bot
@@ -13,19 +14,28 @@ class moderation(commands.Cog):
     # Create the text channel #moderation if it doesnt exist.
     @commands.Cog.listener()
     async def on_ready(self):
+        """Create the text channel"""
         for guild in self.bot.guilds:
             channel = nextcord.utils.get(guild.channels, name="moderation")
             if not channel:
                 overwrites = {
-                    guild.default_role: nextcord.PermissionOverwrite(read_messages=False),
-                    nextcord.utils.get(guild.roles, name="Admin"): nextcord.PermissionOverwrite(read_messages=True)
-                }
+        guild.default_role: nextcord.PermissionOverwrite(read_messages=False),
+        nextcord.utils.get(guild.roles, name="Admin"):
+            nextcord.PermissionOverwrite(read_messages=True)
+    }
                 await guild.create_text_channel("moderation", overwrites=overwrites)
 
-    
+
     # Slash command that will delete all posts by users on the server.
     @nextcord.slash_command(description="Delete posts by a user.")
-    async def message_purge(self, ctx: Interaction, user: nextcord.Member, limit: str, channel: nextcord.TextChannel = None):
+    async def message_purge(
+        self,
+        ctx: Interaction,
+        user: nextcord.Member,
+        limit: str,
+        channel: nextcord.TextChannel = None
+    ):
+        """Delete posts by a user."""
         if ctx.user.guild_permissions.administrator:
             if limit.lower() == 'all':
                 channel = ctx.channel if channel is None else channel
@@ -38,23 +48,41 @@ class moderation(commands.Cog):
                 await ctx.send(f"Deleted last {limit} posts by {user.mention} in {channel.name}.")
         else:
             await ctx.send("You lack permissions for this command.")
-    
-    # A listener that will look in all messages sent in the server. If the message contains a link, it will post in  the server chanel #moderation who sent the link, at what time, and the link itself.
+
+    # A listener that will look in all messages sent in the server.
+    # If the message contains a link, it will post in  the server chanel #moderation
+    # who sent the link, at what time, and the link itself.
     # This will be sent as an embed.
     @commands.Cog.listener()
     async def on_message(self, message):
+        """Check if message contains a link, and post it in #moderation."""
         if not message.author.bot:
             if message.channel.name != "moderation":
                 if "http" in message.content or message.content.startswith("www"):
-                    moderation_channel = nextcord.utils.get(message.guild.channels, name="moderation")
+                    moderation_channel = nextcord.utils.get(
+                        message.guild.channels,
+                        name="moderation"
+                    )
                     if moderation_channel:
-                        embed = nextcord.Embed(title="Link posted", description=f"User {message.author.mention} posted a link in {message.channel.mention} at {message.created_at}.", color=0x00ff00)
+                        embed = nextcord.Embed(
+                            title="Link posted",
+                            description=(
+                                f"User {message.author.mention} posted a link in "
+                                f"{message.channel.mention} at {message.created_at}."
+                            ),
+                            color=0x00ff00
+                        )
                         embed.add_field(name="Link", value=message.content, inline=False)
                         # Add interaction button for "Go to Message".
-                        go_to_message_button = nextcord.ui.Button(style=nextcord.ButtonStyle.link, label="Go to Message", url=message.jump_url)  
+                        go_to_message_button = nextcord.ui.Button(
+                            style=nextcord.ButtonStyle.link,
+                            label="Go to Message",
+                            url=message.jump_url
+                        )
                         view = nextcord.ui.View()
                         view.add_item(go_to_message_button)
                         await moderation_channel.send(embed=embed, view=view)
-                        
+
 def setup(bot):
+    """Add the cog to the bot."""
     bot.add_cog(moderation(bot))
